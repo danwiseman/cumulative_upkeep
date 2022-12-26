80bb5a5c515d1c3fcfd971ea8072d1b3f22bdc5b
danwiseman <dan@thewisemans.io>
2022-12-26 06:09:58 -0500

    updated schema began working on pothos prisma

--- schema.ts
+++ schema.ts
@@ -1,16 +1,27 @@
 import SchemaBuilder from '@pothos/core';
+import { PrismaClient } from '@prisma/client';
+import PrismaPlugin from '@pothos/plugin-prisma';
+// This is the default location for the generator, but this can be
+// customized as described above.
+// Using a type only import will help avoid issues with undeclared
+// exports in esm mode
+import type PrismaTypes from '@pothos/plugin-prisma/generated';
 
-const builder = new SchemaBuilder({});
+const prisma = new PrismaClient({});
 
-builder.queryType({
-    fields: (t) => ({
-        hello: t.string({
-            args: {
-                name: t.arg.string(),
-            },
-            resolve: (parent, { name }) => `hello, ${name || 'World'}`,
-        }),
-    }),
+const builder = new SchemaBuilder<{
+    PrismaTypes: PrismaTypes;
+}>({
+    plugins: [PrismaPlugin],
+    prisma: {
+        client: prisma,
+        // defaults to false, uses /// comments from prisma schema as descriptions
+        // for object types, relations and exposed fields.
+        // descriptions can be omitted by setting description to false
+        //exposeDescriptions: boolean | { models: boolean, fields: boolean },
+        // use where clause from prismaRelatedConnection for totalCount (will true by default in next major version)
+        filterConnectionTotalCount: true,
+    },
 });
 
 export const schema = builder.toSchema();
