072fe62e52a383323d1d08b9a018efc9ecccd3c2
danwiseman <dan@thewisemans.io>
2022-12-27 03:07:45 -0500

    working version that filters cards

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,76 @@
-import { createServer } from 'node:http'
-import { createYoga } from 'graphql-yoga'
-import { schema } from './schema'
+// npm install @apollo/server graphql
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { Resolvers } from './generated/graphql.js'
 
-// Create a Yoga instance with a GraphQL schema.
-const yoga = createYoga({ schema })
+import { PrismaClient } from '@prisma/client'
 
-// Pass it into a server to hook into request handlers.
-const server = createServer(yoga)
+const prisma = new PrismaClient();
 
-// Start the server and you're done!
-server.listen(4000, () => {
-    console.info('Server is running on http://localhost:4000/graphql')
-})
+import { readFileSync } from 'fs';
+const typeDefs = readFileSync('./schema/schema.graphql', { encoding: 'utf-8' });
+
+const resolvers: Resolvers = {
+    Query: {
+        allWeatherCards: () => {
+            return prisma.weatherCard.findMany();
+        },
+        weatherCardsByTag: (_, args) => {
+            return prisma.weatherCard.findMany(
+                {
+                    where: {
+                    weather_tags: {
+                        has: args.tagsList
+                    }
+                }
+            });
+
+        },
+        weatherCardsByTemp: (_, args) => {
+            return prisma.weatherCard.findMany(
+                {
+                    where: {
+                        temperature_range: {
+                            is: {
+                                min: { lte: args.temperature },
+                                max: { gte: args.temperature }
+                            }
+                        }
+                    }
+                });
+
+        },
+        weatherCardsByTagsAndTemp: (_, args) => {
+            return prisma.weatherCard.findMany(
+                {
+                    where: {
+                        weather_tags: {
+                            has: args.tagsList
+                        },
+                        temperature_range: {
+                            is: {
+                                min: { lte: args.temperature },
+                                max: { gte: args.temperature }
+                            }
+                        }
+                    }
+                });
+
+        },
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+
+// Passing an ApolloServer instance to the `startStandaloneServer` function:
+//  1. creates an Express app
+//  2. installs your ApolloServer instance as middleware
+//  3. prepares your app to handle incoming requests
+const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
